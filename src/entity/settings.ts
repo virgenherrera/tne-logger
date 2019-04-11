f2fe84d2cfbe8292335d2a1f92acb615923315bf
Hugo Virgen <hugo_virgen@epam.com>
2019-04-11 17:03:40 -0500

    commitlog
    - updpate package metadata
    - npm audit fix
    - npm update
    - decrease code branches entity/settings , lib/setup
    - keep 7 day logfiles
    - code coverage to 100%

--- src/entity/settings.ts
+++ src/entity/settings.ts
@@ -1,7 +1,7 @@
 import * as Transport from 'winston-transport';
 import { ISettings, IFileSettings } from '../interface';
 import { Format } from 'logform';
-import { Setup } from '../lib/setup';
+import { parseFormat, parseLogLevel, parseFileConfig, parseTransports } from '../lib/setup';
 
 export class Settings {
 	public format: Format;
@@ -9,11 +9,13 @@ export class Settings {
 	public fileCfg: IFileSettings;
 	public transports: Transport[];
 
-	constructor(args: ISettings = {}) {
-		this.format = Setup.parseFormat(args);
-		this.level = Setup.parseLogLevel(args);
-		this.fileCfg = Setup.parseFileConfig(args);
-		this.transports = Setup.parseTransports(args, this.format, this.fileCfg);
+	constructor(args: ISettings) {
+		args = <ISettings>new Object(args);
+
+		this.format = parseFormat(args);
+		this.level = parseLogLevel(args);
+		this.fileCfg = parseFileConfig(args);
+		this.transports = parseTransports(args, this.format, this.fileCfg);
 
 		Object.freeze(this);
 	}
--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -7,57 +7,50 @@ import { consoleTransport } from '../transport/console';
 import { fileTransport } from '../transport/file';
 import { parse } from 'path';
 
-export class Setup {
-	public static parseFormat(args: ISettings = {}): Format {
-		const { format = LOG_FORMAT } = args;
+export function parseFormat(args: ISettings): Format {
+	const { format = LOG_FORMAT } = args;
 
-		return format;
+	return format;
+}
+
+export function parseLogLevel({ level = '' }: ISettings): string {
+
+	return (level in LogLevel)
+		? LogLevel[level]
+		: LogLevel.debug;
+}
+
+export function parseFileConfig(args: ISettings): IFileSettings {
+	if (!args.fileCfg || !args.fileCfg.logsPath) {
+		return null;
 	}
 
-	public static parseLogLevel(args: ISettings = null): string {
-		const level = (args && args.level) ? args.level : '';
+	const {
+		logsPath,
+		datePattern = FILE_DATE_PATTERN,
+	} = args.fileCfg;
+	const { name, ext } = parse(args.fileCfg.logFile || FILE_NAME);
+	const logFile = `${name}${ext ? ext : '.log'}`;
 
-		return (level in LogLevel)
-			? LogLevel[level]
-			: LogLevel.debug;
-	}
-
-	public static parseFileConfig(args: ISettings = null): IFileSettings {
-		if (!args || !args.fileCfg || !args.fileCfg.logsPath) {
-			return null;
-		}
-
-		const {
-			logsPath,
-			datePattern = FILE_DATE_PATTERN,
-		} = args.fileCfg;
-		const { name, ext } = parse(args.fileCfg.logFile || FILE_NAME);
-		const logFile = `${name}${ext ? ext : '.log'}`;
-
-		return { logsPath, logFile, datePattern };
-	}
+	return { logsPath, logFile, datePattern };
+}
 
 
-	public static parseTransports(args: ISettings, format: Format, fileCfg: IFileSettings = null): Transport[] {
-		const customTransports = (args && args.customTransports && Array.isArray(args.customTransports)) ? args.customTransports : [];
-		const transports = [];
+export function parseTransports(args: ISettings, format: Format, fileCfg: IFileSettings = null): Transport[] {
+	const customTransports = (args && args.customTransports && Array.isArray(args.customTransports)) ? args.customTransports : [];
+	const transports: Transport[] = [consoleTransport(format)];
 
+	if (fileCfg) {
 		transports.push(
-			consoleTransport(format)
+			fileTransport(fileCfg)
 		);
+	}
 
-		if (fileCfg) {
-			transports.push(
-				fileTransport(fileCfg)
-			);
+	return customTransports.reduce((acc, transport) => {
+		if (transport instanceof Transport) {
+			acc.push(transport);
 		}
 
-		customTransports.forEach((v, k) => {
-			if (v instanceof Transport) {
-				transports.push(customTransports[k]);
-			}
-		});
-
-		return transports;
-	}
+		return acc;
+	}, transports);
 }
